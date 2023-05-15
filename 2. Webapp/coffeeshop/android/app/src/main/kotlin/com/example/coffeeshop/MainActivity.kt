14d85e500831cb3779c7ebff754a47196fc6c43f
sonthb3992 <71615947+sonthb3992@users.noreply.github.com>
2023-05-15 23:56:39 +0700

    Revert "Session1.2"

--- 2. Webapp/coffeeshop/android/app/src/main/kotlin/com/example/coffeeshop/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.coffeeshop
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 2. Webapp/coffeeshop/lib/domain/concrete/breakfast_order.dart
+++
@@ -1,7 +0,0 @@
-import 'package:coffeeshop/domain/concrete/enums.dart';
-
-import './order.dart';
-
-class BreakfastOrder extends Order {
-  BreakfastOrder(MenuOption menuOption) : super(menuOption);
-}
